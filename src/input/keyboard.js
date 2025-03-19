0416fd52c767b7633e7b904af8c7948ac8cba581
dmil <dmil@itu.dk>
2025-03-19 16:55:23 +0100

    add console logging on every key press

--- src/input/keyboard.js
+++ src/input/keyboard.js
@@ -1,6 +1,17 @@
 let DOWN = {};
-onkeydown = e => DOWN[e.keyCode] = true;
-onkeyup = e => DOWN[e.keyCode] = false;
+
+// Original keyboard event handlers with simple console logging
+onkeydown = e => {
+    if (!DOWN[e.keyCode]) {
+        console.log(`Key pressed: ${e.keyCode} (${e.key})`);
+    }
+    DOWN[e.keyCode] = true;
+};
+
+onkeyup = e => {
+    console.log(`Key released: ${e.keyCode} (${e.key})`);
+    DOWN[e.keyCode] = false;
+};
 
 // Reset inputs when window loses focus
 onblur = onfocus = () => {
