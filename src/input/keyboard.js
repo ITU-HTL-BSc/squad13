facf82dbe6377786ffdf6b863f9ebb6cac7b5723
Daniel Millard <dmil@itu.dk>
2025-04-02 14:58:22 +0200

    Remove unnecessary return, move game logic in event handlers

--- src/input/keyboard.js
+++ src/input/keyboard.js
@@ -2,29 +2,27 @@ let DOWN = {};
 const id = Math.random().toString(36).substring(2, 15);
 
 const sendLog = async (level, msg) => {
-  const response = await fetch("http://localhost:3000/log", {
+  await fetch("http://localhost:3000/log", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ level: level, id: id, msg: msg }),
   });
-  return response;
 };
 
-// Original keyboard event handlers with simple console logging
 onkeydown = async (e) => {
   if (!DOWN[e.keyCode]) {
+    DOWN[e.keyCode] = true;
     console.log(`Key pressed: ${e.code} (${e.keyCode})`);
     await sendLog("info", `Key pressed: ${e.code} (${e.keyCode})`);
   }
-  DOWN[e.keyCode] = true;
 };
 
 onkeyup = async (e) => {
+  DOWN[e.keyCode] = false;
   console.log(`Key released: ${e.code} (${e.keyCode})`);
   await sendLog("info", `Key released: ${e.code} (${e.keyCode})`);
-  DOWN[e.keyCode] = false;
 };
 
 // Reset inputs when window loses focus
@@ -32,7 +30,3 @@ onblur = onfocus = () => {
   DOWN = {};
   MOUSE_RIGHT_DOWN = MOUSE_DOWN = false;
 };
-
-// while (true) {
-//     sendLog("info", "test");
-// }
